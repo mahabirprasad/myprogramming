4a3761502660cd7506dc1b2231266475fb5401d9
Mahabir Prasad <Mahabir.Prasad@INFOVISIONLABS.COM>
2023-01-23 21:31:41 +0530

    d class added

---
+++ JavaPgm/src/main/java/com/java/D.java
@@ -0,0 +1,14 @@
+package com.java;
+/*
+*@author: Mahabir Prasad Mohapatra
+*/
+public class D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mahabir");
+
+	}
+
+}
