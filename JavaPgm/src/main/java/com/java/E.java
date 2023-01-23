ef72bae32f1f136119f35e908c1adb35d7274ec1
Mahabir Prasad <Mahabir.Prasad@INFOVISIONLABS.COM>
2023-01-23 21:39:08 +0530

    commited E class

---
+++ JavaPgm/src/main/java/com/java/E.java
@@ -0,0 +1,12 @@
+package com.java;
+/*
+*@author: Mahabir Prasad Mohapatra
+*/
+public class E {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Mahabir Prasad Mohapatra");
+	}
+
+}
